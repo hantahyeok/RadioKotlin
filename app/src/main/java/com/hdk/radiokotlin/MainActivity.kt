75b4fe82d630308325663ebf6d8691622879f337
hantahyeok <hantahyeok@naver.com>
2023-07-04 23:19:39 +0900

    bindservice 추가 했는데 MainAcitivy 상호작용 어려움..

--- app/src/main/java/com/hdk/radiokotlin/MainActivity.kt
+++ app/src/main/java/com/hdk/radiokotlin/MainActivity.kt
@@ -2,15 +2,20 @@ package com.hdk.radiokotlin
 
 import android.animation.ValueAnimator
 import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.ComponentName
 import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.content.ServiceConnection
 import android.content.pm.PackageManager
 import android.media.AudioFormat
 import android.media.AudioManager
 import android.media.AudioTrack
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.IBinder
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -25,6 +30,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.hdk.radiokotlin.adapter.MyAdapter
 import com.hdk.radiokotlin.data.RadioStation
 import com.hdk.radiokotlin.databinding.ActivityMainBinding
+import com.hdk.radiokotlin.databinding.FragmentRadioBinding
 import com.hdk.radiokotlin.fragment.BookMarkFragment
 import com.hdk.radiokotlin.fragment.PodcastFragment
 import com.hdk.radiokotlin.fragment.RadioFragment
@@ -34,6 +40,7 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
+    private var isServiceRunning: Boolean = false
 
     // DBHelper 객체 선언
     private lateinit var dbHelper: DBHelper
@@ -41,6 +48,7 @@ class MainActivity : AppCompatActivity() {
     private var currentFragment: Fragment? = null
 
     lateinit var binding: ActivityMainBinding
+    lateinit var binding2: FragmentRadioBinding
 
     private val radioFragment by lazy { RadioFragment() }
     private val bookMarkFragment by lazy { BookMarkFragment() }
@@ -52,20 +60,44 @@ class MainActivity : AppCompatActivity() {
     var isPlay: Boolean = false
     var isStar: Boolean = false
 
-    lateinit var favicon : String
-    lateinit var name : String
-    lateinit var url : String
-    lateinit var url_resolved : String
+    lateinit var favicon: String
+    lateinit var name: String
+    lateinit var url: String
+    lateinit var url_resolved: String
 
     var items = mutableListOf<RadioStation>()
 
+    private var isBound = false
+    private var musicService: MusicService? = null
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder = service as MusicService.MusicBinder
+            musicService = binder.getService()
+            isBound = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            musicService = null
+            isBound = false
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val filter = IntentFilter("com.hdk.radiokotlin.MUSIC_STATUS")
+
+        val intent = Intent(this, MusicService::class.java)
+        bindService(intent, connection, Context.BIND_AUTO_CREATE)
 
         // DBHelper 객체 초기화
         dbHelper = DBHelper(this)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding2 = FragmentRadioBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
         mediaPlayer = MediaPlayer()
@@ -78,24 +110,25 @@ class MainActivity : AppCompatActivity() {
         //supportFragmentManager.beginTransaction().add(R.id.fl_con, NaviHomeFragment()).commit()
 
         bnv_main.setOnItemSelectedListener { item ->
-                when (item.itemId) {
-                    R.id.action_radio -> {
-                        changeFragment(radioFragment)
-                        // Respond to navigation item 1 click
-                    }
-                    R.id.action_bookmark -> {
-                        changeFragment(bookMarkFragment)
-                        // Respond to navigation item 2 click
-                    }
+            when (item.itemId) {
+                R.id.action_radio -> {
+                    changeFragment(radioFragment)
+                    // Respond to navigation item 1 click
+                }
+
+                R.id.action_bookmark -> {
+                    changeFragment(bookMarkFragment)
+                    // Respond to navigation item 2 click
+                }
 //                    R.id.action_podcast -> {
 //                        changeFragment(podcastFragment)
 //                        // Respond to navigation item 3 click
 //                    }
-                    R.id.action_setup -> {
-                        changeFragment(settingFragment)
-                        // Respond to navigation item 4 click
-                    }
+                R.id.action_setup -> {
+                    changeFragment(settingFragment)
+                    // Respond to navigation item 4 click
                 }
+            }
             true
         }
         bnv_main.selectedItemId = R.id.action_radio
@@ -104,7 +137,7 @@ class MainActivity : AppCompatActivity() {
 
             items.add(RadioStation(name, favicon, url, url_resolved))
 
-            if(isStar == false){
+            if (isStar == false) {
 
                 // 즐겨찾기 삭제
 //                delete()
@@ -116,7 +149,7 @@ class MainActivity : AppCompatActivity() {
                 animator.start()
                 isStar = true
 
-            }else{
+            } else {
 //
 //                //즐겨찾기 추가
 //                save()
@@ -135,54 +168,33 @@ class MainActivity : AppCompatActivity() {
 
         binding.playBtn.setOnClickListener {
 
-//            stop()
-            if(isPlay == false){ // stop..
+            mediaPlayer.stop()
 
-                mediaPlayer.stop()
-                mediaPlayer.reset()
+            if (isPlay == false) { // stop..
 
                 binding.progressBar.visibility = View.INVISIBLE
+                musicService?.stopMusic()
 
                 val animator = ValueAnimator.ofFloat(0.5f, 1f).setDuration(1000)
                 animator.addUpdateListener { animation ->
                     binding.playBtn.setProgress(animation.animatedValue as Float)
                 }
                 animator.start()
+
                 isPlay = true
 
-            }else{ // play..
+            } else { // play..
 
-                mediaPlayer.stop()
-                mediaPlayer.reset()
 
                 binding.progressBar.visibility = View.VISIBLE
-
-
-//                val intent : Intent = Intent(this, MusicService::class.java)
-//                intent.putExtra("url", url)
-//                startService(intent)
-
-                try {
-                    mediaPlayer.setDataSource(url)
-                    mediaPlayer.setOnPreparedListener { mp ->
-                        mp.setVolume(1.0f, 1.0f)
-                        mp.start() // 준비가 완료되면 재생 시작
-                        Toast.makeText(this, "음악 플레이중...", Toast.LENGTH_SHORT).show()
-                        binding.progressBar.visibility = View.INVISIBLE
-                    }
-                    mediaPlayer.prepareAsync()
-                } catch (e: IOException) {
-                    Log.e("MediaPlayer", "Failed to set data source: ${e.message}")
-                    e.printStackTrace()
-                    binding.progressBar.visibility = View.INVISIBLE
-
-                }
+                musicService?.startMusic(url)
 
                 val animator = ValueAnimator.ofFloat(0f, 0.5f).setDuration(1000)
                 animator.addUpdateListener { animation ->
                     binding.playBtn.setProgress(animation.animatedValue as Float)
                 }
                 animator.start()
+
                 isPlay = false
             }
 
@@ -190,12 +202,12 @@ class MainActivity : AppCompatActivity() {
 
     }// onCreate...
 
-    fun save(){
+    fun save() {
 //        val sharedPreferences = applicationContext.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE)
 //        val editor = sharedPreferences.edit()
     }
 
-    fun delete(){
+    fun delete() {
 
     }
 
@@ -224,31 +236,13 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun getMedia(favicon: String, name: String, url: String, url_resolved: String){
+    fun getMedia(favicon: String, name: String, url: String, url_resolved: String) {
         binding.progressBar.visibility = View.VISIBLE
 
         mediaPlayer.stop()
         mediaPlayer.reset()
 
-        try {
-            mediaPlayer.setDataSource(url)
-            mediaPlayer.setOnPreparedListener { mp ->
-                mp.setVolume(1.0f, 1.0f)
-                mp.start() // 준비가 완료되면 재생 시작
-                Toast.makeText(this, "음악 플레이중...", Toast.LENGTH_SHORT).show()
-                binding.progressBar.visibility = View.INVISIBLE
-            }
-            mediaPlayer.prepareAsync()
-        } catch (e: IOException) {
-            binding.progressBar.visibility = View.INVISIBLE
-            Log.e("MediaPlayer", "Failed to set data source: ${e.message}")
-            e.printStackTrace()
-        }
-
-//        val intent = Intent(this, MusicService::class.java)
-//        intent.putExtra("url", url)
-//        startService(intent)
-
+        musicService?.startMusic(url)
 
         val animator = ValueAnimator.ofFloat(0f, 0.5f).setDuration(1000)
         animator.addUpdateListener { animation ->
@@ -271,6 +265,13 @@ class MainActivity : AppCompatActivity() {
             .error(R.drawable.noimage) // 에러 시 대체 이미지 리소스
             .into(binding.iv)
         binding.tv.text = name
+    }// getMedia...
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // 서비스와 언바인드
+        unbindService(connection)
     }
+
 
 }
--- app/src/main/java/com/hdk/radiokotlin/MusicService.kt
+++ app/src/main/java/com/hdk/radiokotlin/MusicService.kt
@@ -1,8 +1,10 @@
 package com.hdk.radiokotlin
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
 import android.view.View
@@ -16,49 +18,67 @@ import kotlin.math.log
 class MusicService : Service() {
 
 
-    lateinit var mediaPlayer : MediaPlayer
-    lateinit var url : String
-
-    override fun onBind(intent: Intent?): IBinder? {
-        return null
-    }
+    lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate() {
         super.onCreate()
-
-        var intent : Intent = Intent()
-        url = intent?.getStringExtra("url").toString()
-
-        Log.i("dahyeok", url)
-
         mediaPlayer = MediaPlayer()
 
-        Toast.makeText(this, "$url", Toast.LENGTH_SHORT).show()
-
-        try {
-            mediaPlayer.setDataSource(url)
-            mediaPlayer.setOnPreparedListener { mp ->
-                mp.setVolume(1.0f, 1.0f)
-                mp.start() // 준비가 완료되면 재생 시작
-                Toast.makeText(this, "음악 플레이중...", Toast.LENGTH_SHORT).show()
-            }
-            mediaPlayer.prepareAsync()
-        } catch (e: IOException) {
-            Log.e("MediaPlayer", "Failed to set data source: ${e.message}")
-            e.printStackTrace()
-        }
-
-        }
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        mediaPlayer = MediaPlayer.create(this, R.raw.mp3)
-        mediaPlayer.start()
 
         return START_STICKY
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        mediaPlayer.release()
     }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return MusicBinder()
+    }
+
+    inner class MusicBinder : Binder() {
+        fun getService(): MusicService {
+            return this@MusicService
+        }
+    }
+
+    fun startMusic(url: String) {
+        if (!mediaPlayer.isPlaying) {
+
+            mediaPlayer.stop()
+            mediaPlayer.reset()
+
+            try {
+                    mediaPlayer.setDataSource(url)
+                    mediaPlayer.setOnPreparedListener { mp ->
+                        mp.setVolume(1.0f, 1.0f)
+                        mp.start() // 준비가 완료되면 재생 시작
+                        Toast.makeText(this, "음악 플레이중...", Toast.LENGTH_SHORT).show()
+                    }
+                    mediaPlayer.prepareAsync()
+                } catch (e: IOException) {
+                    Log.e("MediaPlayer", "Failed to set data source: ${e.message}")
+                    e.printStackTrace()
+                }
+        }
+    }
+
+    // 음악 정지 메서드
+    fun stopMusic() {
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.stop()
+        }
+    }
+
+    // MainActivity로 메시지를 보내는 함수
+//    private fun play(isPlaying: Boolean) {
+//        val intent = Intent(this, MainActivity::class.java)
+//        intent.putExtra("play", isPlaying)
+//
+//    }
 
 }
